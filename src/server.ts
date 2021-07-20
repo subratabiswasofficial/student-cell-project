b65e65d31be948471237d61ccb8c7bb202c5cb2c
Subrata Biswas <subratabiswasofficial@gmail.com>
2021-07-20 13:49:01 +0530

    connecting fe be

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import logging from './config/logging';
 import config from './config/config';
 import routes from './routes';
 import db from './config/db';
+import path from 'path';
 
 const NAMESPACE = 'Server';
 const app = express();
@@ -38,10 +39,19 @@ app.use((req, res, next) => {
 app.use('/api', routes);
 
 /** Error Handler */
-app.use('*', (req, res) => {
-    logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]`);
-    return res.status(404).json({ message: 'Not found' });
-});
+// app.use('*', (req, res) => {
+//     logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]`);
+//     return res.status(404).json({ message: 'Not found' });
+// });
+
+/** Only for heroku build */
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+    });
+}
 
 /** App listener */
 app.listen(`${config.server.port}`, () => {
